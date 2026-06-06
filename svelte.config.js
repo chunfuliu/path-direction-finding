01132938249d87f507d113b89b72d280171c29c0
dlm589 <chunfu.liu12@gmail.com>
2026-06-06 16:42:35 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
-
+const dev = "production" === "development";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/path-direction-finding",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+    }
 };
 
 export default config;
