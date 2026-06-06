37b5a4fa215e77a3dbfd2e7ba91453943d82a08c
dlm589 <chunfu.liu12@gmail.com>
2026-06-06 17:04:14 -0400

    update build

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-const dev = "production" === "development";
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            // change below to your repo name
-            base: dev ? "" : "/path-direction-finding",
-        },
-        // hydrate the <div id="svelte"> element in src/app.html
-    }
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
